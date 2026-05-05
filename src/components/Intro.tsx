090634bbeb58cf7809f119a65655bae218aa19c0
Maciej Wasiak <hello@maciejwasiak.com>
2026-05-05 12:16:21 +0200

    cache service + CSP update

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,118 +11,147 @@ import TransformImage from "./ui/effects/TransformImage";
 import TypeText, { TypeStep } from "./ui/effects/TypeText";
 import Title from "./ui/Title";
 import UnderlineLink from "./ui/UnderlineLink";
+import { cacheService } from "../utils/cache.service";
+
+interface IntroData {
+  played: boolean;
+  skipped: boolean;
+}
 
 interface IntroProps {
-	close: () => void;
+  close: () => void;
 }
 
 export default function Intro({ close }: IntroProps) {
-	const { pause, player } = useUtil();
-	const [typeSteps, setTypeSteps] = useState<TypeStep[]>([
-		{ text: "Space", pause: true },
-		{ text: "The final frontier.", pause: true },
-		{
-			text: "We stand today on the edge of a new frontier.",
-			pause: true,
-		},
-		{
-			text: "Beyond that frontier are uncharted areas of opportunities.",
-			pause: true,
-		},
-		{ text: "AI Automation" },
-		{ text: "Autonomous agents" },
-		{ text: "Gen AI workflows" },
-		{ text: "AI medical diagnosis" },
-		{ text: "Improving decision-making" },
-		{ text: "Want to know more?" },
-	]);
-	const [typeDelay, setTypeDelay] = useState(500);
-	const [typing, setTyping] = useState(true);
-	const [skipped, setSkipped] = useState(false);
-	const [closing, setClosing] = useState(false);
-	const [blackOutActive, setBlackOutActive] = useState(false);
-	const [imageTransformSteps, setImageTransformSteps] = useState<
-		AnimationStep[]
-	>([
-		{
-			action: "moveUp",
-			delay: 3000,
-		},
-	]);
-	const [spacemenSteps, setSpacemenSteps] = useState<AnimationStep[]>([
-		{ action: "moveDown", delay: 1000 },
-	]);
+  const { pause, player } = useUtil();
+  const [typeSteps, setTypeSteps] = useState<TypeStep[]>([
+    { text: "Space", pause: true },
+    { text: "The final frontier.", pause: true },
+    {
+      text: "We stand today on the edge of a new frontier.",
+      pause: true,
+    },
+    {
+      text: "Beyond that frontier are uncharted areas of opportunities.",
+      pause: true,
+    },
+    { text: "AI Automation" },
+    { text: "Autonomous agents" },
+    { text: "Gen AI workflows" },
+    { text: "AI medical diagnosis" },
+    { text: "Improving decision-making" },
+    { text: "Want to know more?" },
+  ]);
+  const [typeDelay, setTypeDelay] = useState(500);
+  const [typing, setTyping] = useState(true);
+  const [skipped, setSkipped] = useState(false);
+  const [closing, setClosing] = useState(false);
+  const [blackOutActive, setBlackOutActive] = useState(false);
+  const [imageTransformSteps, setImageTransformSteps] = useState<
+    AnimationStep[]
+  >([
+    {
+      action: "moveUp",
+      delay: 3000,
+    },
+  ]);
+  const [spacemenSteps, setSpacemenSteps] = useState<AnimationStep[]>([
+    { action: "moveDown", delay: 1000 },
+  ]);
+  const introState = cacheService.getData<IntroData>("intro") ?? {
+    played: false,
+    skipped: false,
+  };
 
-	async function handleClose() {
-		if (typing || skipped) {
-			return;
-		}
-		setClosing(true);
-		setImageTransformSteps([
-			{
-				action: "scaleUp",
-				delay: 1000,
-			},
-		]);
-		setSpacemenSteps([{ action: "moveUp" }]);
-		setTypeDelay(0);
-		setTypeSteps([{ text: "Excellent! Get ready!" }]);
-		await pause(2000);
-		await handleSkip();
-	}
+  async function handleStart() {
+    if (typing || skipped) {
+      return;
+    }
+    setClosing(true);
+    setImageTransformSteps([
+      {
+        action: "scaleUp",
+        delay: 1000,
+      },
+    ]);
+    setSpacemenSteps([{ action: "moveUp" }]);
+    setTypeDelay(0);
+    setTypeSteps([{ text: "Excellent! Get ready!" }]);
 
-	async function handleSkip() {
-		setSkipped(true);
-		blackOut();
-		player.volumeDown();
-		await pause(1000);
-		return close();
-	}
+    introState.played = true;
+    cacheService.set("intro", introState);
 
-	function blackOut() {
-		setBlackOutActive(true);
-	}
+    await pause(2000);
+    await handleClose();
+  }
 
-	async function playAudio() {
-		player.load(audioTheme);
-		await pause(1000);
-		player.play(0.1);
-	}
+  async function handleSkip() {
+    setSkipped(true);
 
-	useEffect(() => {
-		playAudio();
+    introState.skipped = true;
+    cacheService.set("intro", introState);
 
-		return () => {
-			player.pause();
-		};
-	}, []);
+    await handleClose();
+  }
 
-	return (
-		<div className="relative w-screen h-screen overflow-hidden">
-			<IntroTopbar skip={() => handleSkip()} />
-			<SpaceBackground />
-			<TransformImage src={imgEarth} steps={imageTransformSteps} />
-			<div className="relative md:absolute top-60 md:left-60 z-10 w-4/6 md:w-1/2 mx-auto">
-				<Title>
-					<TypeText
-						typeSteps={typeSteps}
-						delay={typeDelay}
-						skipped={skipped}
-						setTyping={setTyping}
-					/>
-				</Title>
-				{!typing && !skipped && !closing && (
-					<UnderlineLink
-						label="Let's start!"
-						onClick={handleClose}
-						fadeIn={true}
-					/>
-				)}
-			</div>
-			<div className="-ml-1">
-				<BlackOut active={blackOutActive} mode="out" />
-			</div>
-			<Spaceman position={["top-20", "right-40"]} steps={spacemenSteps} />
-		</div>
-	);
+  async function handleClose() {
+    blackOut();
+    player.volumeDown();
+
+    await pause(1000);
+    return close();
+  }
+
+  function blackOut() {
+    setBlackOutActive(true);
+  }
+
+  async function playAudio() {
+    player.load(audioTheme);
+    await pause(1000);
+    player.play(0.1);
+  }
+
+  useEffect(() => {
+    if (introState.skipped) {
+      blackOut();
+      return close();
+    }
+
+    playAudio();
+
+    return () => {
+      player.pause();
+    };
+  }, []);
+
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <IntroTopbar skip={() => handleSkip()} />
+      <SpaceBackground />
+      <TransformImage src={imgEarth} steps={imageTransformSteps} />
+      <div className="relative md:absolute top-60 md:left-60 z-10 w-4/6 md:w-1/2 mx-auto">
+        <Title>
+          <TypeText
+            typeSteps={typeSteps}
+            delay={typeDelay}
+            skipped={skipped}
+            setTyping={setTyping}
+          />
+        </Title>
+        {!typing && !skipped && !closing && (
+          <UnderlineLink
+            label="Let's start!"
+            onClick={handleStart}
+            fadeIn={true}
+          />
+        )}
+      </div>
+      <div className="-ml-1">
+        <BlackOut active={true} mode="in" />
+        <BlackOut active={blackOutActive} mode="out" />
+      </div>
+      <Spaceman position={["top-20", "right-40"]} steps={spacemenSteps} />
+    </div>
+  );
 }
---
+++ src/utils/cache.service.ts
@@ -0,0 +1,51 @@
+type CacheKey = "at" | "rt" | "user" | "intro";
+
+export interface CacheItem {
+  value: string;
+  expire: number | undefined;
+}
+
+const cacheService = {
+  prefix: "mw-",
+  defaultExpire: 30 * 24 * 60 * 60 * 1000,
+
+  get(key: CacheKey): CacheItem | null {
+    const data = localStorage.getItem(`${this.prefix}${key}`);
+
+    if (!data) {
+      return null;
+    }
+
+    const item: CacheItem = JSON.parse(data);
+
+    return this.validate(key, item);
+  },
+
+  getData<T>(key: CacheKey): T | null {
+    const cached = this.get(key);
+    return cached ? (JSON.parse(cached.value) as T) : null;
+  },
+
+  set(key: CacheKey, value: any, expire?: number): void {
+    const item = {
+      value: JSON.stringify(value),
+      expire: Date.now() + (expire ?? this.defaultExpire),
+    };
+    const data = JSON.stringify(item);
+    localStorage.setItem(`${this.prefix}${key}`, data);
+  },
+
+  del(key: CacheKey): void {
+    localStorage.removeItem(`${this.prefix}${key}`);
+  },
+
+  validate(key: CacheKey, item: CacheItem): CacheItem | null {
+    if (item.expire && item.expire <= Date.now()) {
+      localStorage.removeItem(key);
+      return null;
+    }
+    return item;
+  },
+};
+
+export { cacheService };
